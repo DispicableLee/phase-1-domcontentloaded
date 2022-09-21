96463f78c9eda9116affc0ec4cb96d8ba1d791e8
DispicableLee <lee.robert053@gmail.com>
2022-09-21 18:20:30 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+function changeContent(){
+  const changed = document.getElementById("text")
+  changed.textContent = "Wow this is cool!"
+}
+
+document.addEventListener("DOMContentLoaded", function(e) {
+    console.log("The DOM has loaded");
+    changeContent();
+  });
+  
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+
